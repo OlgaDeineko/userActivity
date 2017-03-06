c09bb4a4c412e30b2d4dc64fee3341071f4f8a05
olga_deineko <olga_deineko@ukr.net>
2017-03-06 02:22:47 +0200

    delete all user activity

--- app/user-activity-list/user-activity-list.component.js
+++ app/user-activity-list/user-activity-list.component.js
@@ -4,17 +4,26 @@ angular.module('userActivityList', []).component('userActivityList', {
   templateUrl: './app/user-activity-list/user-activity-list.template.html',
   controller: ['User', function UserActivityListController(user) {
     var self = this;
-    user.getUserActivity().then(function (response) {
-      self.userActivity = response.data;
-    });
+    self.getAllUserActivity = function () {
+      user.getUserActivity().then(function (response) {
+        self.userActivity = response.data;
+        return self.userActivity
+      });
+    }
+    self.getAllUserActivity();
 
     self.deleteAllUserActivity = function () {
-      user.deleteUserActivity();
-    }
+      user.deleteUserActivity().success(function (data, status, headers) {
+        self.ServerResponse = status;
+        self.getAllUserActivity();
+        console.log(self.ServerResponse);
+      })
+        .error(function (data, status, header, config) {
+          self.ServerResponse = status;
+          console.log(self.ServerResponse);
+        });
 
-    //   .then(function (response) {
-    //   self.delet = response.data;
-    // });
+    }
 
   }]
 });
